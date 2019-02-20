f50c96bc012b37410730705dd317607068520dd6
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2019-02-20 01:59:54 +0100

    Small fixes for this change
    * Removed redundant code.
    * Renamed function to better reflect its behaviour.
    * Marked `noDisable` and `noMobile` as optional.

--- types/easymde.d.ts
+++ types/easymde.d.ts
@@ -81,8 +81,8 @@ declare namespace EasyMDE {
         action: string|((editor: EasyMDE) => void);
         className: string;
         title: string;
-        noDisable: boolean;
-        noMobile: boolean;
+        noDisable?: boolean;
+        noMobile?: boolean;
     }
 
     interface Options {
