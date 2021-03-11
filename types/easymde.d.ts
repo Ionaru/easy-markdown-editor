6e7e8f9b8220d7b9a787a614b301ccbf8c2d9eea
Samson Zhang <wwsamson@yahoo.com>
2021-03-11 12:09:09 -0700

    add imagePathAbsolute option

--- types/easymde.d.ts
+++ types/easymde.d.ts
@@ -207,6 +207,7 @@ declare namespace EasyMDE {
         imageAccept?: string;
         imageUploadFunction?: (file: File, onSuccess: (url: string) => void, onError: (error: string) => void) => void;
         imageUploadEndpoint?: string;
+        imagePathAbsolute?: boolean;
         imageCSRFToken?: string;
         imageTexts?: ImageTextsOptions;
         errorMessages?: ImageErrorTextsOptions;
