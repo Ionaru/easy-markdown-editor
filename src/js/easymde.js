5612108008556697bd95fb2d4fc1c4e8bd653818
Sam Bennett <bennettsst@gmail.com>
2023-10-10 10:24:38 +0100

    Add method to allow the adding of custom bindings

--- src/js/easymde.js
+++ src/js/easymde.js
@@ -2875,6 +2875,9 @@ EasyMDE.prototype.value = function (val) {
     }
 };
 
+EasyMDE.prototype.addBinding = function(name, method) {
+    bindings[name] = method;
+};
 
 /**
  * Bind static methods for exports.
